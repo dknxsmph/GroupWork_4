76c0811e2fcd9b524511b292cf63d1054d2e2fea
nortorkor <immintchyy@gmail.com>
2021-10-26 23:10:39 +0700

    up

--- WorkShop6/ProductList.js
+++ WorkShop6/ProductList.js
@@ -1,1 +1,96 @@
-function prod(productId, productName, price, stocks)
+import { product } from "./Product"
+
+function prod() {
+    const p001 = document.querySelector('#P001');
+    p001.addEventListener('click', addToCart, false);
+    cart.load();
+}
+prod();
+addProduct(
+    'P002',
+    'LINNMON',
+    'Desk, white100x60 cm',
+    749,
+    'img/2.png',
+    8
+);
+addProduct(
+    'P003',
+    'SKÅDIS ',
+    'Pegboard, white76x56 cm',
+    650,
+    'img/3.png',
+    20
+);
+addProduct(
+    'P004',
+    'CHILISTRÅN',
+    'Hanging planter, in/outdoor white12 cm',
+    299,
+    'img/4.png',
+    15
+);
+addProduct(
+    'P005',
+    'FEJKA',
+    'Artificial potted plant, in/outdoor Monstera9 cm',
+    249,
+    'img/5.png',
+    14
+);
+addProduct(
+    'P006',
+    'LAUTERS',
+    'Floor lamp, ash/white',
+    1990,
+    'img/6.png',
+    4
+);
+addProduct(
+    'P007',
+    'TÅRBÄK',
+    'Rug, flatwoven, handmade/multicolour170x240 cm',
+    7990,
+    'img/7.png',
+    19
+);
+addProduct(
+    'P008',
+    'BLÅHAJ',
+    'Soft toy, shark100 cm',
+    590,
+    'img/8.png',
+    16
+);
+addProduct(
+    'P009',
+    'VRETSTORP',
+    'Cover for 3-seat sofa-bed, Hallarp grey',
+    16990,
+    'img/9.png',
+    16
+);
+addProduct(
+    'P010',
+    'UPPTÅG',
+    'Cushion, red45x27 cm',
+    390,
+    'img/10.png',
+    16
+);
+addProduct(
+    'P011',
+    'POÄNG',
+    'Armchair cushion, Knisa light beige',
+    3590,
+    'img/11.png',
+    16
+);
+addProduct(
+    'P012',
+    'NISSAFORS',
+    'Trolley, black50.5x30x83 cm',
+    890,
+    'img/12.png',
+    16
+);
