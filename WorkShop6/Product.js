fec1c2c6a52a812d53927ee6c1ba5c2a2e1106d7
nortorkor <immintchyy@gmail.com>
2021-10-28 21:45:01 +0700

    sfd

--- WorkShop6/Product.js
+++ WorkShop6/Product.js
@@ -1,9 +1,27 @@
 let product = [{
-    productId: 'P001',
-    productName: 'MATCHSPEL',
-    productDesc: 'Gaming chair, Bomstad black',
-    price: 4990,
-    img: 'img/1.png',
-    stocks: 10
-}, ];
-export { product };
+        productId: 'P001',
+        productName: 'MATCHSPEL',
+        productDesc: 'Gaming chair, Bomstad black',
+        price: 4990,
+        img: 'img/1.png',
+        stocks: 10
+    },
+    {
+        productId: 'P001',
+        productName: 'MATCHSPEL',
+        productDesc: 'Gaming chair, Bomstad black',
+        price: 4990,
+        img: 'img/1.png',
+        stocks: 10
+    },
+
+
+    {
+        productId: 'P001',
+        productName: 'MATCHSPEL',
+        productDesc: 'Gaming chair, Bomstad black',
+        price: 4990,
+        img: 'img/1.png',
+        stocks: 10
+    },
+];
