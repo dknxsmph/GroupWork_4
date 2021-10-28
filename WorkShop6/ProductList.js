ef20210fba0151f035774e112933aaf71ac3c1e9
nortorkor <immintchyy@gmail.com>
2021-10-28 21:36:41 +0700

    sfd

--- WorkShop6/ProductList.js
+++ WorkShop6/ProductList.js
@@ -1,11 +1,52 @@
-import { product } from "./Product"
+// import { product } from "./Product"
 
-function prod() {
-    const p001 = document.querySelector('#P001');
-    p001.addEventListener('click', addToCart, false);
-    cart.load();
+function addProduct(id, name, desc, price, img, stock, ) {
+    let productList = document.querySelector("#product");
+    let div_col = document.createElement("div"); // create div_col parent 
+    div_col.setAttribute("class", "col pb-2");
+    let card = document.createElement("div"); //create card child div_col
+    card.setAttribute("class", "card shadow");
+    card.setAttribute("style", "width: 100%");
+    let card_body = document.createElement("div"); // child card
+    card_body.setAttribute("class", "card-body");
+    let h_title = document.createElement("h5"); // child card
+    h_title.setAttribute("class", "card-title m-2");
+    h_title.innerHTML = name;
+    let p_text = document.createElement("p"); // child card 
+    p_text.setAttribute("class", "card-text m-1");
+    p_text.innerHTML = desc;
+    let p_price = document.createElement("p"); // child card 
+    p_price.setAttribute("class", "price-text m-1");
+    p_price.innerHTML = "Price: " + price;
+    let p_stock = document.createElement("p"); // child card 
+    p_stock.setAttribute("class", "stock-text m-1");
+    p_stock.innerHTML = "Stock: " + stock;
+    let div_img = document.createElement("img"); // child card
+    div_img.setAttribute("src", img);
+    div_img.setAttribute("height", "300px");
+    let b_btn = document.createElement("a"); // child card
+    b_btn.setAttribute("href", "#");
+    b_btn.setAttribute("class", "btn btn-secondary my-3")
+    b_btn.setAttribute("style", "width: 100%");
+    b_btn.innerHTML = "Buy";
+    productList.appendChild(div_col);
+    div_col.appendChild(card);
+    card_body.appendChild(h_title);
+    card_body.appendChild(p_text);
+    card.appendChild(card_body);
+    card_body.appendChild(div_img);
+    card_body.appendChild(p_price);
+    card_body.appendChild(p_stock);
+    card_body.appendChild(b_btn);
 }
-prod();
+addProduct(
+    'P001',
+    'MATCHSPEL',
+    'gaming chair Bomstad black',
+    4990,
+    'img/1.png',
+    3
+);
 addProduct(
     'P002',
     'LINNMON',
@@ -33,7 +74,7 @@ addProduct(
 addProduct(
     'P005',
     'FEJKA',
-    'Artificial potted plant, in/outdoor Monstera9 cm',
+    'Artificial potted plant, in/outdoor Monstera 9 cm',
     249,
     'img/5.png',
     14
