2dedf6574347c50c6d8b341326e063e07cc4a119
Ziim-eiei <simsimsim21@gmail.com>
2021-11-22 11:29:02 +0700

    edit cart.js

--- WorkShop6/cart.js
+++ WorkShop6/cart.js
@@ -6,43 +6,47 @@ amount.innerHTML = countCart();
 const show = document.querySelector("#total");
 
 function addToCart(id) {
-    const select = product.find((p) => p.productId == id);
-    alert(`${select.productName} has been added to your shopping cart`);
-    const check = cart.find((c) => c.productId == id);
-    if (check) {
-        check.amount++;
-    } else {
-        cart.push({
-            productId: select.productId,
-            amount: 1,
-        });
-    }
-    amount.innerHTML = countCart();
-    console.log(cart);
-    localStorage.setItem("cart", JSON.stringify(cart));
+  alert(`${select.productName} has been added to your shopping cart`);
+  const check = cart.find((c) => c.productId == id);
+  if (check) {
+    check.amount++;
+  } else {
+    cart.push({
+      productId: id,
+      amount: 1,
+    });
+  }
+  amount.innerHTML = countCart();
+  console.log(cart);
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 function total() {
-    return cart.reduce((acc, cur) => acc + cur.amount * product.find(p => p.productId == cur.productId).price, 0);
+  return cart.reduce(
+    (acc, cur) =>
+      acc +
+      cur.amount * product.find((p) => p.productId == cur.productId).price,
+    0
+  );
 }
 
 function update(id, item_amount) {
-    item_amount = parseInt(item_amount);
-    const item = cart.find((c) => c.productId == id);
-    item.amount = item_amount;
-    show.innerHTML = "Total : " + total();
-    localStorage.setItem("cart", JSON.stringify(cart));
-    amount.innerHTML = countCart();
+  item_amount = parseInt(item_amount);
+  const item = cart.find((c) => c.productId == id);
+  item.amount = item_amount;
+  show.innerHTML = "Total : " + total();
+  localStorage.setItem("cart", JSON.stringify(cart));
+  amount.innerHTML = countCart();
 }
 
 function clearCart() {
-    cart = [];
-    amount.innerHTML = countCart();
-    localStorage.setItem("cart", JSON.stringify([]));
+  cart = [];
+  amount.innerHTML = countCart();
+  localStorage.setItem("cart", JSON.stringify([]));
 }
 
 function countCart() {
-    return cart.reduce((acc, cur) => acc + cur.amount, 0);
+  return cart.reduce((acc, cur) => acc + cur.amount, 0);
 }
 
 export { addToCart, cart, total, update, clearCart };
